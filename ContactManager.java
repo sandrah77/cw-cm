577399d356dbc3a4fcb2f2d86281585b434c5432
BBK-PiJ-2015-29 <ocouls01@dcs.bbk.ac.uk>
2015-12-15 16:23:53 +0000

    Made first interface for ContactManager, starting initially with the addNewContact() method.

---
+++ ContactManager.java
@@ -0,0 +1,20 @@
+/**
+ * Interface for the ContactManager
+ *
+ * @author ocouls01
+ */
+
+public interface ContactManager {
+	/**
+	 * Create a new contact with the specified name and notes.
+	 *
+	 * @param name the name of the contact.
+	 * @param notes notes to be added about the contact.
+	 * @return the ID for the new contact
+	 * @throws IllegalArgumentException if the name or the notes are empty strings
+	 * @throws NullPointerException if the name or the notes are null
+	 */
+	int addNewContact(String name, String notes);
+
+	
+}
