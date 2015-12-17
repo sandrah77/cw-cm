02443ee9e38f5c6c97996db84d97a49f1a3810a6
BBK-PiJ-2015-29 <ocouls01@dcs.bbk.ac.uk>
2015-12-17 19:32:54 +0000

    Created junit test class for the FutureMeeting and Meeting interfaces, as FutureMeeting has no extra methods, this test class will test the methods in the abstract class MeetingImpl which is extended by FutureMeetingImpl

---
+++ FutureMeetingTest.java
@@ -0,0 +1,51 @@
+/**
+ * JUnit4 tests for the FutureMeeting interface
+ *
+ * @author ocouls01
+ */
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+public class FutureMeetingTest {
+	private FutureMeeting testFutureMeeting;
+	private Calendar testFutureDate;
+	private Set<Contact> testSet;
+	
+	@Before
+	public void setUp() {
+		testSet = new HashSet<Contact>();
+		testSet.add(new ContactImpl(1, "Albert Einstein"));
+		testSet.add(new ContactImpl(2, "Alan Turing"));
+		testSet.add(new ContactImpl(3, "Richard Feynman"));
+		
+		testFutureDate = new GregorianCalendar(2020, 5, 13, 13, 0);
+		testFutureMeeting = new FutureMeetingImpl(29, testFutureDate, testSet);
+		
+	}
+	
+	@After
+	public void breakDown() {
+		testFutureDate = null;
+		testSet = null;
+		testFutureMeeting = null;
+		
+	}
+	
+	@Test
+	public void testConstruction() {
+		FutureMeeting output = new FutureMeeting(1, testFutureDate, testSet);
+		assertNotNull(output);
+		
+		assertEquals(1, output.getId());
+		assertEquals(testFutureDate, output.getDate());
+		assertEquals(testSet, output.getContacts());
+		
+	}
+}
