050332b50bdb0633cd4d31060b2f81895a10bdea
BBK-PiJ-2015-29 <ocouls01@dcs.bbk.ac.uk>
2015-12-17 20:29:07 +0000

    Added code to PastMeetingImpl so all tests pass

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -8,9 +8,16 @@ import java.util.Calendar;
  */
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+	private String notes;
 	
 	public PastMeetingImpl(int ID, Calendar date, Set<Contact> contacts, String notes) {
 		super(ID, date, contacts);
+		
+		if (notes == null) {
+			throw new NullPointerException("Must provide a notes String");
+		} else {
+			this.notes = notes;
+		}
 	}
 	/**
 	 * Returns the notes from the meeting.
@@ -20,6 +27,6 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	 * @return the notes from the meeting.
 	 */
 	public String getNotes() {
-		return null;
+		return notes;
 	}
 }
